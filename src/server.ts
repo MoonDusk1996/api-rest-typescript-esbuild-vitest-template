c2bf35b47e768c3b83f82ee8a7b3ea20a58d275c
Washington Lopes <washington.lopesdasilvafilho@gmail.com>
2023-03-02 19:24:12 -0400

    refactore: port

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,6 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!');
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
 });
